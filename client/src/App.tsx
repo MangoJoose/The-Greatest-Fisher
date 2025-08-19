227cc5cddfcca52a5d6d45bef2b7e12a9e30e195
MangoJoose <matthewl1401@gmail.com>
2025-08-19 10:15:03 -0400

    Added a Base Button Component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import './App.css';
+import Button from "./components/Button";
 
 function App() {
+
+  const testAlert = () => {
+    alert("Test Alert");
+  };
+
   return (
     <div className="App">
       <header className="App-header">
+        <Button label="Fish" onClick={testAlert} />
       </header>
     </div>
   );
