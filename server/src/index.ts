bde7097ad8e69e886637f671e7d7ea181f53e6fb
MangoJoose <matthewl1401@gmail.com>
2025-08-27 22:11:13 -0400

    Separated DB logic from index

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,34 +1,13 @@
 import express from 'express';
 import path from 'path';
-import dotenv from 'dotenv';
-dotenv.config();
 import { load_game_data, game_data } from "./config_loader";
 import apiRoutes from "./routes/api";
-
-import { Pool } from 'pg';
-
-export const pool = new Pool({
-    user: process.env.PG_USER,
-    host: process.env.PG_HOST,
-    database: process.env.PG_DB,
-    password: process.env.PG_PASSWORD,
-    port: 5432,
-});
 
 const app = express();
 const port = 5000;
 
 app.use(express.json());
 app.use("/api", apiRoutes);
-
-async function getFish() {
-    try {
-        const res = await pool.query('SELECT * FROM fish');
-        console.log(res.rows);
-    } catch (err) {
-        console.error('Error fetching fish:', err);
-    }
-}
 
 // Handlebars
 app.set('view engine', 'hbs');
@@ -61,5 +40,4 @@ load_game_data();
 
 app.listen(port, () => { // Run Server with npm run dev
     console.log(`Server is running at http://localhost:${port}`);
-    getFish();
 });
