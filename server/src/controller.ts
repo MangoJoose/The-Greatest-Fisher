0960c17df50f8a754fa02f25b1e3d4885a7127a9
MangoJoose <matthewl1401@gmail.com>
2025-08-27 21:47:16 -0400

    Added another JSON to handle rarity magic numbers

--- server/src/controller.ts
+++ server/src/controller.ts
@@ -1,23 +1,27 @@
 import { Request, Response } from "express";
 import { game_data } from "./config_loader";
 import { Fish } from "./config_loader";
+import config from "../game_data/config.json";
 
 export async function go_fish(req: Request, res: Response) {
     try {
+        
         console.log("Controller Contacted");
         const rand: number = Math.random();
         let fish_table: Fish[] = [];
         if (rand < 0.9) { // Common
-            fish_table = game_data.fish.filter(f => f.rarity == 1);
+            fish_table = game_data.fish.filter(f => f.rarity == config.COMMON);
             console.log("Common fished");
         } else {
-            fish_table = game_data.fish.filter(f => f.rarity == 2);
+            fish_table = game_data.fish.filter(f => f.rarity == config.UNCOMMON);
             console.log("Uncommon Fished");
         }
+
         const fish_rand: number = Math.random();
         const amount_of_fish: number = fish_table.length;
         let chance: number = 1 / amount_of_fish;
         let fished_fish = 0;
+
         for (let i=0; i<amount_of_fish; i++) {
             if (fish_rand < chance) {
                 fished_fish = i;
@@ -25,8 +29,10 @@ export async function go_fish(req: Request, res: Response) {
             }
             chance += chance;
         }
+
         console.log(fish_table[fished_fish]);
         res.json(fish_table[fished_fish]);
+
     } catch (err) {
         console.error(err);
         res.status(500).send("Error Fishing");
